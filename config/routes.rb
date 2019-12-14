870a30d9906f1337e26a9fbcaaaaeaa1419a3ba9
Daichi Kamiyama <dawt2h@gmail.com>
2019-12-14 19:49:23 +0900

    fix/rooting_navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  get '/libraries', to: 'libraries#index'
   resources :libraries
   resources :posts
   resources :books
   resources :users
-  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
