c412d46f0d360e146070479b26e9fe9c58e36816
Daichi Kamiyama <dawt2h@gmail.com>
2019-12-24 22:42:39 +0900

    fix login_logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && !!@user.authenticate(params[:session][:password])
       log_in @user
+      current_user
       redirect_to root_path
     else
       flash[:danger] = "メールまたはパスワードが正しくありません"
